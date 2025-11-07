83e8fa474d7630e67c8850cd1fa08c9d1777f8bc
Gianluca GL <grancicg@gmail.com>
2025-11-06 21:58:45 -0300

    wip: budget input modal

--- script.js
+++ script.js
@@ -1,26 +1,100 @@
 const btnPresupuesto = document.querySelector('#btnPresupuesto');
 const btnGasto = document.querySelector('#btnGasto');
-const lblPres = document.querySelector('#lblPresupuesto')
 
-const modal = document.querySelector('#myModal');
+const expenseForm = document.querySelector('form');
+
+const lblPres = document.querySelector('#lblPresupuesto')
+const lblTG = document.querySelector('#lblTG');
+const lblProm = document.querySelector('#lblProm');
+
+const expensesModal = document.querySelector('#expensesModal');
+const budgetModal = document.querySelector('#budgetModal');
+
+const anyModal = document.querySelector('.modal');
+
+
 const closeBtn = document.querySelector('.close');
 
+const cancelBtn = document.querySelector('#cancelBtn');
+const saveBtn = document.querySelector('#saveBtn');
 
-let presupuesto;
+const expensesTable = document.querySelector('table');
+
+
+let presupuesto = 0;
+let totalGastado = 0;
+let promedio = 0;
+let expensesQty = 0;
+
+
+function addRow(inData) {
+
+  const row = document.createElement('tr');
+
+  for(prop in inData) {
+    const rowData = document.createElement('td');
+    rowData.innerText = inData[prop];
+    row.appendChild(rowData);
+  }
+
+  expensesTable.appendChild(row);
+
+}
 
 btnPresupuesto.addEventListener('click', () => {
-
-  presupuesto = parseFloat(prompt('Ingresa el presupuesto'));
-  lblPres.textContent = `$${presupuesto}`;
-  btnGasto.disabled = false;
-  btnPresupuesto.disabled = true;
-
+ budgetModal.style.display = 'block';
 });
 
 btnGasto.addEventListener('click', () => {
-  modal.style.display = 'block';
+  expensesModal.style.display = 'block';
 });
 
 closeBtn.addEventListener('click', () => {
-  modal.style.display = 'none';
+  anyModal.style.display = 'none';
+});
+
+cancelBtn.addEventListener('click', () => {
+  anyModal.style.display = 'none';
+});
+
+budgetModal.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const budgetAmt = document.querySelector('#budgetAmt');
+  presupuesto = budgetAmt.value;
+
+})
+
+expenseForm.addEventListener('submit', (event) => {
+
+  // dont refresh the webpage after submitting.
+  event.preventDefault();
+
+  let expenseDateIn = document.querySelector('#expenseDate');
+  let descriptionIn = document.querySelector('#expenseDescription');
+  let categoryIn = document.querySelector('#combo');
+  let ammountIn = document.querySelector('#expenseAmt');
+
+  const expense = {
+    date: expenseDateIn.value,
+    description: descriptionIn.value,
+    category: categoryIn.value,
+    ammount: ammountIn.value,
+  };
+
+  addRow(expense);
+
+  totalGastado += parseFloat(ammountIn.value);
+  lblTG.textContent = `$${totalGastado}`;
+
+  expensesQty++
+  promedio = totalGastado / expensesQty;
+  lblProm.textContent = `$${promedio}`;
+
+  expenseDateIn.value = '';
+  descriptionIn.value = '';
+  categoryIn.value = '';
+  ammountIn.value = '';
+
+  anyModal.style.display = 'none';
+
 });
